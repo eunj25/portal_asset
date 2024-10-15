95f262a57458de6d7491815fc60d245758b78d5e
eunj25 <s_unju0205@naver.com>
2024-10-15 16:27:29 +0900

    '2nd'

--- script/custom.js
+++ script/custom.js
@@ -24,10 +24,14 @@ window.addEventListener('load', () => {
 // pagetitle
 const pageTitles = {
   asset: 'RPA Portal design asset',
-  TaskDefine: '자동화업무 제안/선정'
+  TaskDefine: '자동화업무 제안/선정',
+  TaskDefineCreate: '자동화업무 제안/선정',
+  TaskDeployConfirm: '배포실행/승인'
 };
 const pageHint = {
-  TaskDefine: '목록'
+  TaskDefine: '목록',
+  TaskDefineCreate: '자동화업무 제안 작성',
+  TaskDeployConfirm: '구현 된 자동화업무 운영이관 승인'
 };
 
 function updateTitle(pageKey) {
@@ -42,3 +46,22 @@ function updateTitle(pageKey) {
     hintElement.textContent = 'Default Page Title';
   }
 }
+
+// teb_menu
+document.addEventListener("DOMContentLoaded", function() {
+  const tabButtons = document.querySelectorAll(".tab_button");
+  const tabContents = document.querySelectorAll(".tab_content");
+
+  function switchTab(event) {
+    tabButtons.forEach(button => button.classList.remove("active"));
+    tabContents.forEach(content => content.classList.remove("active"));
+
+    const targetContent = document.getElementById(event.currentTarget.dataset.tab);
+
+    event.currentTarget.classList.add("active");
+    targetContent.classList.add("active");
+  }
+
+  tabButtons.forEach(button => button.addEventListener("click", switchTab));
+});
+
